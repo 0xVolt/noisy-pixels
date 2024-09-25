2c3ddfa54e0420cb21d4ee775734a5438202c9b2
Desh Iyer <desh.iyer@gmail.com>
2024-09-25 11:25:13 +0400

    Create function to randomize circle's x

--- sketches/perlin-noise-basics.js
+++ sketches/perlin-noise-basics.js
@@ -1,14 +1,26 @@
-// This is a sketch to explore noise and subsequently Perlin noise
+// This is a sketch to visualize the difference between randomness and noise
 
 function setup() {
+  // Setup the size of the canvas
   createCanvas(600, 600);
+}
+
+function drawCircleWithRandomX() {
+  // Shift the origin to the center of the screen
+  translate(width / 2, height / 2);
+
+  // Have the horizontal position of the circle be a random value every frame
+  var x = random(-width + 50, width - 50);
+
+  
+  // Draw the circle
+  noFill();
+  stroke(255);
+  ellipse(x, -75, 25, 25);
 }
 
 function draw() {
   background(51);
   
-  translate(width / 2, height / 2);
-  noFill();
-  stroke(255);
-  ellipse(0, 0, 25, 25);
+  drawCircleWithRandomX();
 }
