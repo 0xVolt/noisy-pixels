90236b64997659b477a29a37d2a55604b17a97c0
Desh Iyer <desh.iyer@gmail.com>
2024-09-25 11:38:56 +0400

    Update function to determine a noise value for x

--- sketches/perlin-noise-basics.js
+++ sketches/perlin-noise-basics.js
@@ -1,5 +1,7 @@
 // This is a sketch to visualize the difference between randomness and noise
 
+// There are two functions that draw a circle on the canvas. One where the circle's x position is random every frame and the other where the x value is determined by a noise value every time you refresh the sketch.
+
 function setup() {
   // Setup the size of the canvas
   createCanvas(600, 600);
@@ -10,17 +12,31 @@ function drawCircleWithRandomX() {
   translate(width / 2, height / 2);
 
   // Have the horizontal position of the circle be a random value every frame
-  var x = random(-width + 50, width - 50);
-
+  var x = random(-width, width);
   
   // Draw the circle
-  noFill();
-  stroke(255);
   ellipse(x, -75, 25, 25);
+}
+
+
+function drawCircleWithNoisyX() {
+  // Shift the origin to the center of the screen
+  translate(width / 2, height / 2);
+
+  // Have the horizontal position of the circle be a random value every frame
+  var seed = 100;
+  // Map the noise value for the given seed (which ranges from 0 to 1) to a value between the left and right edges of the screen
+  var x = map(noise(seed), 0, 1, -width, width);
+
+  // Draw the circle
+  ellipse(x, 75, 25, 25);
+  console.log(x);
 }
 
 function draw() {
   background(51);
   
-  drawCircleWithRandomX();
+  // Try both of these functions and see how the result changes. Every time you refresh the sketch window, a new distribution for the noise values is created. That's why there's only one circle that can be drawn.
+  // drawCircleWithRandomX();
+  drawCircleWithNoisyX();
 }
