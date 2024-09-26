b8fffeed82d0c07e81474343f9f1af2e7a898e78
Desh Iyer <desh.iyer@gmail.com>
2024-09-26 15:59:47 +0400

    Add comments

--- sketches/2-graphing-perlin-noise.js
+++ sketches/2-graphing-perlin-noise.js
@@ -1,16 +1,15 @@
 // This sketch intends to develop further on simply moving a circle along one axis with noise to graphing noise in two dimensions
 
-// We use 2 noise offset values to keep the perlin noise space the same but take two different values from it
-var noiseOffset1 = 0;
-var noiseOffset2 = 1000;
-
 function setup() {
   createCanvas(400, 400);
 }
 
-function draw() {
-  background(55);
-  
+// We use 2 noise offset values to keep the perlin noise space the same but take two different values from it
+var noiseOffset1 = 0;
+var noiseOffset2 = 1000;
+
+// Function to draw an ellipse every frame where the x and y positions of the circle change with a single noise space
+function drawCircleNoisy() {
   // Using the two offsets from the same noise space to generate noisy x and y values
   var x = map(noise(noiseOffset1), 0, 1, 0, width);
   var y = map(noise(noiseOffset2), 0, 1, 0, height);
@@ -19,4 +18,10 @@ function draw() {
   noiseOffset2 += 0.01;
 
   ellipse(x, y, 24, 24);
+}
+
+function draw() {
+  background(55);
+  
+  // drawCircleNoisy();
 }
