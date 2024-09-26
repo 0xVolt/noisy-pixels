d680e60880c651582e6374016ed31902dd2459f6
Desh Iyer <desh.iyer@gmail.com>
2024-09-26 17:45:52 +0400

    Update `noiseOffset` declaration

--- sketches/3-moving-graph.js
+++ sketches/3-moving-graph.js
@@ -2,8 +2,8 @@
 // In this sketch, we explore what it would look like if we were to scan through the perlin noise space
 // We would do this by changing our frame of reference and having the increment to the argument to the noise function, to be variable as well
 
-var increment = 0.1;
-var noiseOffset = 100;
+var increment = 0.01;
+var start = 0;
 
 function setup() {
   createCanvas(600, 600);
@@ -11,11 +11,13 @@ function setup() {
 
 function draw() {
   background(51);
-
+  
   noFill();
   stroke(255);
-
+  
   beginShape();
+  // It's important that this line is in the draw() function and not declared globally
+  var noiseOffset = start;
   for (var x = 0; x < width; ++x) {
     var y = map(noise(noiseOffset), 0, 1, 0, height);
 
@@ -25,5 +27,7 @@ function draw() {
   }
   endShape();
 
-  noLoop();
+  start += increment;
+
+  // noLoop();
 }
