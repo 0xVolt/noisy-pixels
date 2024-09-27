7dc1dd2ff2883ed0f17ec7bbe4f2324261620e51
Desh Iyer <desh.iyer@gmail.com>
2024-09-27 15:48:16 +0400

    Add comments

--- sketches/4-two-dimensional-perlin-noise.js
+++ sketches/4-two-dimensional-perlin-noise.js
@@ -1,3 +1,7 @@
+// This is the sketch that we've been building up to
+// We're going to draw a grid of pixels whose greyscale values correlate to a noise value in x and y dimensions
+// Hopefully, we'll see something cool like a heatmap or a top-down view of noisy terrain 
+
 function setup() {
   createCanvas(400, 400);
   frameRate(5);
