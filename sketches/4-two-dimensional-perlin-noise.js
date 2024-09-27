0ee0ecf51c1088ca6f38b62cb5248e3e8bf8cbc8
Desh Iyer <desh.iyer@gmail.com>
2024-09-27 15:52:04 +0400

    Use `loadPixels()` to directly work with pixels over abstractions to pixels

--- sketches/4-two-dimensional-perlin-noise.js
+++ sketches/4-two-dimensional-perlin-noise.js
@@ -2,11 +2,28 @@
 // We're going to draw a grid of pixels whose greyscale values correlate to a noise value in x and y dimensions
 // Hopefully, we'll see something cool like a heatmap or a top-down view of noisy terrain 
 
+var increment = 0.01;
+
 function setup() {
   createCanvas(400, 400);
   frameRate(5);
 }
 
 function draw() {
+  // Initializing the p5.js pixel array to work with the pixels directly
+  loadPixels();
 
+  background(0);
+  stroke(255);
+
+  noFill();
+  beginShape();
+  for (var x = 0; x < width; ++x) {
+    for (var y = 0; y < height; ++y) {
+
+    }
+  }
+  endShape();
+
+  
 }
